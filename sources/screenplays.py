0785e29a5cb213a923bea717b14b2b25dde73512
Om Prakash C <comprakash@gmail.com>
2021-04-02 16:34:42 +0530

    Updated screenplays code

--- sources/screenplays.py
+++ sources/screenplays.py
@@ -1,8 +1,7 @@
-from bs4 import BeautifulSoup
 import urllib
+import urllib.parse
 import os
 from tqdm import tqdm
-import string
 from .utilities import format_filename, get_soup
 
 
@@ -19,19 +18,19 @@ def get_screenplays():
     movielist = [x for x in mlist if x.get('href').startswith("screenplay")]
 
     for movie in tqdm(movielist):
-        name = format_filename(movie.text)
-        script_url = movie.get('href')
-        # if script_url.startswith("screenplay"):
+        try:
+            name = format_filename(movie.text)
+            script_url = movie.get('href')
+            # if script_url.startswith("screenplay"):
 
-        script_soup = get_soup(BASE_URL + urllib.parse.quote(script_url))
-        # print(script_soup.pre.get_text())
-        if not script_soup.pre:
-            continue
-        text = script_soup.pre.get_text()
+            script_soup = get_soup(BASE_URL + urllib.parse.quote(script_url))
+            # print(script_soup.pre.get_text())
+            if not script_soup.pre:
+                continue
+            text = script_soup.pre.get_text()
 
-        with open(os.path.join(DIR, name + '.txt'), 'w', errors="ignore") as out:
-            out.write(text)
-
-
-
-
+            with open(os.path.join(DIR, name + '.txt'), 'w', errors="ignore") as out:
+                out.write(text)
+        except Exception as ex:
+            print(movie)
+            print(ex)
